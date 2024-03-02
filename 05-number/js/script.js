9491896788de35ea6dac65c7bef1632d3561cb23
Mateus Santos <mateussilvanzg@gmail.com>
2024-03-02 19:40:08 -0300

    Novos exercícios

---
+++ 05-number/js/script.js
@@ -0,0 +1,16 @@
+var num = 5
+console.log(num)
+console.log(typeof num)
+
+var float = 5.25
+console.log(float)
+console.log(typeof float)
+
+var textoComNumero = '512'
+console.log(textoComNumero)
+console.log(typeof textoComNumero)
+
+console.log(NaN)
+console.log(typeof NaN)
+console.log(typeof +Infinity)
+console.log(typeof -Infinity)
---
+++ 06-string/js/script.js
@@ -0,0 +1,15 @@
+var nome = 'Mateus'
+var sobrenome = 'Santos'
+var nomeCompleto = nome + ' ' + sobrenome
+console.log(nome)
+console.log(typeof nome)
+console.log(typeof 'asd')
+console.log(nomeCompleto)
+console.log(typeof '5.24')
+
+var frase = 'Esta é uma frase complexa'
+console.log(frase)
+console.log(typeof frase)
+console.log(nome + ' ' + frase)
+document.write('Ele disse "Olá".')
+console.log("Este número: " + nome)
